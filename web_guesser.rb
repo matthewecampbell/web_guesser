cbdd94ae0ebfe0fe488bededff660bbc385fae08
Matthew Campbell <ematthewcampbell@gmail.com>
2016-06-17 16:09:24 -0600

    done with 1 extension

--- web_guesser.rb
+++ web_guesser.rb
@@ -6,7 +6,8 @@ SECRET_NUMBER = rand(100)
 get '/' do
   guess = params["guess"].to_i
   message = check_guess(guess)
-  erb :index, :locals => {:num => SECRET_NUMBER, :message => message, :user_guess => guess}
+  color = get_color(guess)
+  erb :index, :locals => {:num => SECRET_NUMBER, :message => message, :user_guess => guess, :color => color}
 end
 
 def check_guess(guess)
@@ -20,5 +21,19 @@ def check_guess(guess)
     message = "Too low"
   else
     message = "The secret number is #{SECRET_NUMBER}. You got it right!"
+  end
+end
+
+def get_color(guess)
+  if guess > SECRET_NUMBER + 5
+    color = "red"
+  elsif guess > SECRET_NUMBER
+    color = "lightcoral"
+  elsif guess < SECRET_NUMBER - 5
+    color = "red"
+  elsif guess < SECRET_NUMBER
+    color = "lightcoral"
+  else
+    color = "green"
   end
 end
